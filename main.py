4f1d54b29fc37c8831b555d6b799f0402e8a15ff
Eric Brown <embrown@bridgend.ac.uk>
2025-04-03 14:23:45 +0100

    Fix import

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from google_calendar_service import GoogleCalendarService
+from google_calendar_service import calendar_service
 
 
 if __name__ == "__main__":
-    google_calendar_service = GoogleCalendarService()
-    events = google_calendar_service.get_events(calendar_id="primary", date_from="2025-04-01", date_to="2025-04-01")
+    events = calendar_service.get_events(calendar_id="primary", date_from="2025-04-01", date_to="2025-04-01")
     print(events)
